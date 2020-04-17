791846391a475fc55811f03c796632ef85707800
Nastya Klyashtorna <little.smart.bee@gmail.com>
2020-04-17 22:55:51 +0300

    delete App directory

--- App/js/main.js
+++
@@ -1,3 +0,0 @@
-function noName() {
-    console.log('helooo');  
-}
--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,11 @@ gulp-browserSync
 gulp-concat
 gulp-rename
 gulp-uglify
+gulp-babel
 
 gulp-imagemin
 imagemin-pngquant
+gulp-newer
 */
 
 //Variables
@@ -28,72 +30,79 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     prefixer = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
-    imageminJpegRecompress = require('imagemin-jpeg-recompress');
+    imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+    babel = require('gulp-babel'),
+    newer = require('gulp-newer');
 
     sass.compiler = require('node-sass');
 
 //Path
  let path = {
-    build: { 
-        html: 'build/',
-        js: 'build/js/',
-        style: 'build/css/',
-        image: 'build/image/',
-        fonts: 'build/fonts/'
+    dist: { 
+        html: 'dist/',
+        js: 'dist/js/',
+        style: 'dist/css/',
+        image: 'dist/image/',
+        fonts: 'dist/fonts/'
     },
     src: { 
-        html: 'App/*.html',
-        js: 'App/js/main.js',
-        style: 'App/scss/style.scss',
-        image: 'App/image/**/*.*', 
-        fonts: 'App/fonts/**/*.*'
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/scss/style.scss',
+        image: 'src/image/**/*.*', 
+        fonts: 'src/fonts/**/*.*'
     },
     watch: { 
-        html: 'App/**/*.html',
-        js: 'App/js/**/*.js',
-        style: 'App/scss/**/*.scss',
-        image: 'App/image/**/*.*',
-        fonts: 'App/fonts/**/*.*'
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/scss/**/*.scss',
+        image: 'src/image/**/*.*',
+        fonts: 'src/fonts/**/*.*'
     },
-    clean: './build'
+    clean: './dist'
 };
 
 // Server
 let config = {
     server: {
-        baseDir: "./build"
+        baseDir: "./dist"
     },
     host: 'localhost',
     port: 3000,
-    logPrefix: "App",
+    logPrefix: "src",
 };
 
 // Html handling
 function html() {
     return gulp.src(path.src.html)
-          .pipe(gulp.dest(path.build.html)) 
+          .pipe(gulp.dest(path.dist.html)) 
           .pipe(reload({stream : true}))
 }
 // Style handling, compression, add prefixs
 function style() {
     return gulp.src(path.src.style)
+		.pipe(sourcemaps.init())
          .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
          .pipe(rename({suffix: ".min" }))
          .pipe(prefixer(['last 2 versions'], { cascade: true }))
-         .pipe(gulp.dest(path.build.style))
+         .pipe(sourcemaps.write())
+         .pipe(gulp.dest(path.dist.style))
          .pipe(reload({stream : true}))
 } 
 // Js handling and compression
 function js() {
     return gulp.src(path.src.js)  
-		// .pipe(sourcemaps.init())
+		.pipe(sourcemaps.init())
+        .pipe(babel())
 		.pipe(uglify())
-		.pipe(gulp.dest(path.build.js))
+        .pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.dist.js))
 		.pipe(reload({stream: true}));
 }
 // Images handling and compression
 function image() {
     return gulp.src(path.src.image)
+           .pipe(newer(path.dist.image))
            .pipe(imagemin([
                  imagemin.gifsicle({interlaced: true}),
                  imageminJpegRecompress({
@@ -106,32 +115,33 @@ function image() {
                     speed: 5}),
                 imagemin.svgo({plugins: [{removeViewBox: true}]})
                   ]))
-			.pipe(gulp.dest(path.build.image))
+			.pipe(gulp.dest(path.dist.image))
 			.pipe(reload({stream: true}));
 }
 
 
 function clean() {
-    return del(['build/*'])
+    return del(['dist/*'])
 }
 // Js library handling and concat in one js file
-/*
-function jsLibs(params) {
-    return gulp.src(['src', 'src'])
-         .pipe(concat('libs.min.js'))
-		 .pipe(uglify())
-         .pipe(gulp.dest(path.build.js))
-         .pipe(reload({
-             stream: true
-    }));
-}
-*/
+
+// function jsLibs() {
+//     return gulp.src([src',
+//     'src'])
+//        .pipe(concat('libs.min.js'))
+// 		 .pipe(uglify())
+//          .pipe(gulp.dest(path.dist.js))
+//          .pipe(reload({
+//              stream: true
+//     }));
+// }
+
 
 // Library handling and concat in one js file
 /*
-function fonts(params) {
+function fonts() {
     return gulp.src(path.src.fonts)
-           .pipe(gulp.dest(path.build.fonts))
+           .pipe(gulp.dest(path.dist.fonts))
     });
 }
 */
